8ff5187d98010dcf9992373fcc34900af65b5d72
AzaJon <azamatjon.98@gmail.com>
2020-06-19 00:16:26 +0600

    init auth router

---
+++ App/Controllers/Auth.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Controllers;
+
+use Core\Components\Alert;
+use Core\Components\AlertType;
+use \Core\View;
+
+/**
+ * Auth controller
+ *
+ * PHP version 7.0
+ */
+class Auth extends Base
+{
+
+    /**
+     * Show the login page
+     *
+     * @return void
+     */
+    public function loginAction()
+    {
+        // If the user is already logged in, redirect to referer
+        if ($this->view_data['isLoggedIn']){
+            $this->redirectToReferer();
+        }
+
+        if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+            if (empty($_POST['userName']) || empty($_POST['password'])){
+                $this->alerts['empty_credentials'] = new Alert(
+                    AlertType::DANGER,
+                    'Введите имя пользователя или пароль',
+                    true
+                );
+            } else {
+                $user_name = $_POST['userName'];
+                $password = $_POST['password'];
+
+                if (trim($user_name) == 'admin' && trim($password) == '123'){
+                    $_SESSION['isLoggedIn'] = true;
+
+                    $this->redirectToReferer();
+                } else {
+                    $this->alerts['invalid_credentials'] = new Alert(
+                        AlertType::DANGER,
+                        'Ведите правильные данные',
+                        true
+                    );
+                }
+            }
+        }
+
+        // Show the login template
+        View::renderTemplate('Auth/login.html', $this->view_data);
+    }
+
+    /**
+     * Action for logging out. Redirects to referer after successful cleaning session
+     *
+     * @return void
+     */
+    public function logoutAction()
+    {
+        if(isset($_SESSION['isLoggedIn']))
+            unset($_SESSION['isLoggedIn']);
+
+        $this->redirectToReferer();
+    }
+}
